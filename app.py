9e6878555bd797e68698bfe98b0e1e3e19eb0ad5
Jacob-Campbell <JacobPMCampbell@gmail.com>
2021-12-15 02:35:12 -0600

    done, i'm going to bed

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, jsonify, redirect
+import time
+from random import randint
+import requests
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET', 'POST'])
+
+#define index function
+
+def index():
+    catcall = requests.get('https://aws.random.cat/meow')
+    catlink = catcall.json()["file"]
+
+    catcall2 = requests.get('https://aws.random.cat/meow')
+    catlink2 = catcall2.json()["file"]
+
+    return render_template('index.html',**locals())
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
